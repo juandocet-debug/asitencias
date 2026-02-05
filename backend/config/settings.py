5f9916f6ed46157f9fdcfab3b701829049d47a19
JuanRamirez88 <jdramirez500@misena.edu.co>
2026-02-05 02:10:30 -0500

    Config: Agregado soporte para PostgreSQL (Neon/Render)

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -87,11 +87,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import dj_database_url
+
+# Database
+# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
+        conn_max_age=600
+    )
 }
 
 
