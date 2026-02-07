8ed3b9d3e5beb39405da14fad8c99b20ef702c65
JuanRamirez88 <jdramirez500@misena.edu.co>
2026-02-06 21:02:07 -0500

    Fix: Enable SPA routing and serve index.html

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -38,3 +38,12 @@ from django.conf.urls.static import static
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+from django.urls import re_path
+from django.views.generic import TemplateView
+
+# Serve React App for any other routes (SPA)
+urlpatterns += [
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
+]
+
